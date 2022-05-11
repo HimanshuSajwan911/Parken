50f3bf5b706682831d7396315ea9cc877441d4b6
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2022-05-11 17:07:40 +0530

    added signinUser method to signin user with email and password.

--- app/src/main/java/com/himanshu/parken/LoginActivity.java
+++ app/src/main/java/com/himanshu/parken/LoginActivity.java
@@ -1,14 +1,88 @@
 package com.himanshu.parken;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.himanshu.parken.core.UserProfileActivity;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        etEmail = findViewById(R.id.editText_signin_email);
+        etPassword = findViewById(R.id.editText_signin_password);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar_signin);
+
+        Button btSignin = findViewById(R.id.button_signin_signin);
+        btSignin.setOnClickListener(view -> signinUser());
+
     }
+
+    private void signinUser(){
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Email required!");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Enter a valid Email!");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Password required!");
+            etPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(LoginActivity.this, "incorrect email or password!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
