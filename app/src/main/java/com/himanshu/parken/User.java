c616170c9e1bcfd3afc7adc1cbe6a5d9ea775b1f
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2022-05-11 17:14:18 +0530

    User class to store data of user.

---
+++ app/src/main/java/com/himanshu/parken/User.java
@@ -0,0 +1,66 @@
+package com.himanshu.parken;
+
+import com.google.firebase.database.Exclude;
+
+public class User {
+
+    private String firstName, lastName, email, gender, phone, password;
+
+    public User(String firstName, String lastName, String email, String gender, String phone, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Exclude
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
