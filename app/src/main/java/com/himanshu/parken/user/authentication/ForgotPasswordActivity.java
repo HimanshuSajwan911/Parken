3dfbeec646358dba9ca4d130b973c46e9c1fd031
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2022-05-16 18:13:25 +0530

    added Alert Dialog.

--- app/src/main/java/com/himanshu/parken/user/authentication/ForgotPasswordActivity.java
+++ app/src/main/java/com/himanshu/parken/user/authentication/ForgotPasswordActivity.java
@@ -1,8 +1,6 @@
 package com.himanshu.parken.user.authentication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,6 +11,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,8 +23,6 @@ import java.util.Objects;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
     private EditText etEmail;
-    private Button btResetPassword;
-    FirebaseAuth fbAuth;
     private ProgressBar progressBar;
 
     @Override
@@ -34,16 +33,13 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         etEmail = findViewById(R.id.editText_forgot_password_email);
-        btResetPassword = findViewById(R.id.button_forgot_password_reset);
+        Button btResetPassword = findViewById(R.id.button_forgot_password_reset);
         progressBar = findViewById(R.id.progressBar_forgot_password);
-
-        fbAuth = FirebaseAuth.getInstance();
-
         btResetPassword.setOnClickListener(view -> resetPassword());
 
     }
 
-    private void resetPassword(){
+    private void resetPassword() {
 
         String email = etEmail.getText().toString();
 
@@ -58,19 +54,28 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             return;
         }
 
+        FirebaseAuth fbAuth = FirebaseAuth.getInstance();
+
         progressBar.setVisibility(View.VISIBLE);
 
         fbAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ForgotPasswordActivity.this);
+                builder.setTitle("Password reset email sent");
+                builder.setMessage("check your email for password reset!")
+                        .setCancelable(false)
+                        .setNeutralButton(android.R.string.ok, (dialog, id) -> {
+                            Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+                            dialog.dismiss();
+                            startActivity(intent);
+                            finish();
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            } else {
+                Toast.makeText(ForgotPasswordActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+            }
             progressBar.setVisibility(View.INVISIBLE);
-            if(task.isSuccessful()){
-                Toast.makeText(ForgotPasswordActivity.this, "check your email for password reset!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-            else{
-                Toast.makeText(ForgotPasswordActivity.this, "something went wrong!", Toast.LENGTH_SHORT).show();
-            }
         });
 
     }
